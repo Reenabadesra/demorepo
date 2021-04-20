9d225ed17a33d69e7306f031c4d3399db1b07c10
Reena Badesra <reena.badesra@ibm.com>
2021-04-20 13:45:03 +0530

    Created Atoz and altered Abc and Xyz

--- Abc.java
+++ Abc.java
@@ -3,7 +3,7 @@ public class Abc
 public static void main(String args[])
 {
 for(int i=1;i<=10;i++){
-System.out.println("hello");
+System.out.println("hello everyone there i m here");
 }
 }
 }
---
+++ Atoz.java
@@ -0,0 +1,10 @@
+public void Atoz{
+public void test()
+{
+System.out.println("FromAtoz class");
+}
+public void disp()
+{
+System.out.println("dispfromatozclass");
+}
+}
